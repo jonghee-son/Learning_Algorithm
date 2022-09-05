58d1cb86cdc382bc173e6b3cb4954cbe270cd630
Jonghee Son <jhson@paperecord.com>
2022-09-06 08:17:38 +0900

    modified to use binary search

--- find_number.cpp
+++ find_number.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+int binarySearch(vector<int> &a, int x, int y) {
+    int low = 0, high = y - 1, mid;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x == a.at(mid)) {
+            return 1;
+        }
+        else if (x < a.at(mid)) {
+            high = mid - 1;
+        }
+        else if (x > a.at(mid)) {
+            low = mid + 1;
+        }
+    }
+
+    return 0;
+}
 
 int main() {
     int num_1 = 0;
     int num_2 = 0;
-    int ans = 0;
-    int * arr_1;
+    vector<int> vec;
 
     cin >> num_1;
-    arr_1 = new int[num_1];
 
     for (int i = 0; i < num_1; i++) {
-        cin >> arr_1[i];
+        int input;
+        cin >> input;
+        vec.push_back(input);
     }
+
+    sort(vec.begin(), vec.end());
 
     cin >> num_2;
 
     for (int i = 0; i < num_2; i++) {
-        int flag = 0;
+        int ans = 0;
         cin >> ans;
 
-        for (int j = 0; j < num_1; j++) {
-            if (ans == arr_1[j]) {
-                flag = 1;
-                break;
-            }
-        }
-
-        cout << flag << '\n';
+        cout << binarySearch(vec, ans, num_1) << '\n';
     }
 
     return 0;
