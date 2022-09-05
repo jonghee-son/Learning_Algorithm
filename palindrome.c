1f37ebdedf927a67ab4e2367b73c502f09752feb
Jonghee Son <jhson@paperecord.com>
2022-09-05 09:42:18 +0900

    minor fix

--- palindrome.c
+++ palindrome.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main() {
-    int num, flag = 0;
-    char * str;
-    str = malloc(sizeof(char) * 100000);
+    int num;
+    char str[6];
 
     while (1) {
+        int flag = 1;
+
         scanf("%s", str);
 
         if (str[0] == '0') {
@@ -16,24 +16,19 @@ int main() {
 
         num = strlen(str);
 
-        for (int i = 0; i < num / 2; i++) {
-            if (str[i] == str[num - i - 1]) {
-                flag = 1;
-            }
-            else {
+        for (int i = 0; i < (num / 2); i++) {
+            if (str[i] != str[num - 1 - i]) {
                 flag = 0;
             }
         }
 
-        if (flag == 1) {
+        if (flag == 1 || num == 1) {
             printf("yes\n");
         }
         else {
             printf("no\n");
         }
     }
-
-    free(str);
 
     return 0;
 }
