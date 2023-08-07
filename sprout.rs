851d360715da193eb53a1d9a89314eb115f24b80
Jonghee Son <zzongone@gmail.com>
2023-08-07 23:58:50 +0900

    RE-Organized

--- sprout.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("         ,r'\"7");
-    println!("r`-_   ,'  ,/");
-    println!(" \\. \". L_r'");
-    println!("   `~\\/");
-    println!("      |");
-    println!("      |");
-}
